c9c011ed875e0356069315e531a15ab890ea3aae
pmalloja <mallojalap@gmail.comm>
2025-04-25 11:15:07 -0400

    added user 5

--- main.tf
+++ main.tf
@@ -25,5 +25,6 @@ module "ALL_USERS" {
     TEST_USER2 = { "name" = "TEST_USER2", "first_name" = "test2", "last_name" = "user2", "email" = "user2@snowflake.example", "default_warehouse" = "COMPUTE_WH", "default_role" = "PUBLIC" }
     TEST_USER3 = { "name" = "TEST_USER3", "first_name" = "test3", "last_name" = "user3", "email" = "user3@snowflake.example" }
     TEST_USER4 = { "name" = "TEST_USER4", "first_name" = "test4", "last_name" = "user4", "email" = "user4@snowflake.example" }
+    TEST_USER5 = { "name" = "TEST_USER5", "first_name" = "test5", "last_name" = "user5", "email" = "user5@snowflake.example" }
   }
 }
